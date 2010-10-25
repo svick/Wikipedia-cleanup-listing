b262aa0eceec218e4e32e1da0475a7245cc88b14
Smallman12q <smallman12q@enwiki>
2010-10-25 23:25:32 +0000

    created page for CleanupListing.php...this is beta and currently would only create a table article8 in the db

---
+++ CleanupListing.php
@@ -0,0 +1,142 @@
+<?php
+//Smallman12q
+//PD October 2010
+//Version .1
+//For Cleanup Listing
+ 
+        $cleanupcountercats = array('Articles with invalid date parameter in template', 'Accuracy disputes', 'Articles lacking page references', 'BLP articles lacking sources', 'Unreferenced BLPs', 'Use British English', 'Category needed', 'Articles needing chemical formulas', 'Articles with broken or outdated citations', 'Wikipedia articles needing clarification', 'Wikipedia templates needing cleanup', 'Wikipedia pages needing cleanup', 'Wikipedia categories needing cleanup', 'Articles needing cleanup', 'Statements with common sense issues', 'Wikipedia articles with possible conflicts of interest', 'Wikipedia articles needing context', 'Wikipedia articles needing copy edit', 'Copied and pasted articles and sections', 'Articles containing potentially dated statements', 'Use dmy dates', 'Use mdy dates', 'Use ymd dates', 'Articles with dead external links', 'Dead-end pages', 'Deprecated templates', 'Articles with disputed statements', 'Use British (Oxford) English', 'Articles to be expanded', 'Articles to be expanded by month', 'Articles needing expert attention by month', 'Wikipedia external links cleanup', 'Articles that need to differentiate between fact and fiction', 'Articles sourced only by IMDB', 'Articles sourced by IMDB', 'Articles lacking in-text citations', 'Wikipedia introduction cleanup', 'Articles needing link rot cleanup', 'Articles to be merged', 'Articles with topics of unclear notability', 'NPOV disputes', 'Articles that may contain original research', 'Orphaned articles', 'Articles with minor POV problems', 'Wikipedia articles needing page number citations', 'Articles with peacock terms', 'Wikipedia articles with plot summary needing attention', 'Articles with a promotional tone', 'Articles with sections that need to be turned into prose', 'Articles to be pruned by month', 'Articles pruned by month', 'Articles slanted towards recent events', 'Recently revised', 'Articles needing additional references', 'Articles lacking reliable references', 'Wikipedia articles needing rewrite', 'Articles needing sections', 'Articles with excessive "see also" sections', 'Self-contradictory articles', 'Articles lacking sources', 'Wikipedia spam cleanup', 'Articles to be split', 'Article sections to be split', 'Articles that may be too long', 'Wikipedia articles needing style editing', 'Wikipedia articles that are too technical', 'Articles with trivia sections', 'Uncategorized stubs', 'Unreviewed new articles', 'Unreviewed new articles created via the Article Wizard', 'Articles with unsourced statements', 'Wikipedia articles in need of updating', 'Userspace drafts', 'Userspace drafts created via the Article Wizard', 'Wikipedia articles needing factual verification', 'Articles with weasel words', 'Articles with specifically marked weasel-worded phrases', 'Articles that need to be wikified', 'Wikipedia articles needing SmackBot clean up', 'Wikipedia articles needing cleanup', 'Articles needing the year an event occurred');
+        $months = array("January", "February", "March", "April", "June", "July", "August", "September", "October", "November", "December");
+        $years = range(2004, 2010);
+        $classes = array("FA-Class", "A-Class",    "GA-Class", "B-Class", "C-Class", "Start-Class", "Stub-Class", "FL-Class", "List-Class","Unassessed");
+        $importances = array("Top", "High", "Mid", "Low", "Unknown");
+ 
+ 
+        $wikiprojects = array("Equine");//, "Films", "Food and drink");
+ 
+ 
+        //login
+        $con = mysql_connect("localhost","root","",true) //true?
+                or die('Could not connect: ' . mysql_error());
+ 
+        //create db
+        mysql_query("CREATE DATABASE IF NOT EXISTS enwiki2",$con)
+                or die('Could not create db: ' . mysql_error());
+ 
+        mysql_select_db("enwiki2", $con)
+                or die('Could not select db: ' . mysql_error());
+ 
+ 
+        //foreach as opposed to while for legibility
+        foreach($wikiprojects as $wikiproject)
+        {
+            //Create temporary table
+            $sql = "CREATE TABLE articles8(
+                        pageid INT(8) UNSIGNED,
+                        article VARCHAR(255),
+                        importance VARCHAR(7),
+                        quality VARCHAR(5),
+                        catnumber TINYINT UNSIGNED NOT NULL DEFAULT 0,
+                        taskforce VARCHAR(255),
+                        categories TEXT NOT NULL
+                    )";
+            mysql_query($sql,$con)
+                    or die('Could not create table: ' . mysql_error());
+ 
+            //Load articles and pageid from WikiProject
+            $categoryarticles = "'WikiProject_".$wikiproject."_articles'";
+            $sql = "
+                INSERT INTO articles
+                (
+                    pageid,
+                    article
+                )
+                SELECT page_id as pageid, page_title as article FROM page
+                        JOIN categorylinks AS cl1 ON page.page_id = cl1.cl_from
+                                WHERE cl1.cl_to = ".$categoryarticles.
+                                "AND page.page_namespace = 0";
+            mysql_query($sql,$con)
+                    or die('Could not load WikiProject '.$wikiproject." articles: ". mysql_error());
+ 
+ 
+            //Set importance
+            foreach($importances as $importance)
+            {
+                //try both upper and lower
+                $theimportance = $importance."-importance_".$wikiproject."_articles";
+                //http://www.electrictoolbox.com/article/mysql/cross-table-update/
+                $sql = "UPDATE articles a
+                        LEFT JOIN categorylinks cl
+                        ON a.pageid = cl.cl_from
+                        SET a.importance = '".$importance."'"."
+                        WHERE cl.cl_to = '".$theimportance."'";
+                mysql_query($sql,$con)
+                        or die('Could not load WikiProject '.$wikiproject." importance: ". mysql_error());
+ 
+                    //lowercase
+                $sql = "UPDATE articles a
+                        LEFT JOIN categorylinks cl
+                        ON a.pageid = cl.cl_from
+                        SET a.importance = '".$importance."'"."
+                        WHERE cl.cl_to = '".strtolower($theimportance)."'";
+                mysql_query($sql,$con)
+                        or die('Could not load WikiProject '.$wikiproject." importance: ". mysql_error());
+            }
+ 
+            //Set Class
+            foreach($classes as $class)
+            {
+                                //try both upper and lower - to do
+               $theclass = $class."_".$wikiproject."_articles";
+                $sql = "UPDATE articles a
+                        LEFT JOIN categorylinks cl
+                        ON a.pageid = cl.cl_from
+                        SET a.importance = '".str_replace("-Class","",$class)."'"."
+                        WHERE cl.cl_to = '".$theclass."'";
+            mysql_query($sql,$con)
+                    or die('Could not load WikiProject '.$wikiproject." quality class: ". mysql_error());
+ 
+            //lowercase
+                            $sql = "UPDATE articles a
+                        LEFT JOIN categorylinks cl
+                        ON a.pageid = cl.cl_from
+                        SET a.importance = '".str_replace("-Class","",$class)."'"."
+                        WHERE cl.cl_to = '".strtolower($theclass)."'";
+            mysql_query($sql,$con)
+                    or die('Could not load WikiProject '.$wikiproject." quality class: ". mysql_error());
+ 
+            }
+ 
+            foreach($cleanupcountercats as $countercat)
+            {
+ 
+                //to do create table for each cat
+ 
+                foreach($years as $year)
+                {
+                    foreach($months as $month)
+                    {
+                        $thecountercat =  $countercat." from ".$month." ".$startyear;
+ 
+                        //update main table
+                        $sql = "UPDATE articles a
+                                LEFT JOIN categorylinks cl
+                                ON a.pageid = cl.cl_from
+                                SET a.categories = concat(categories, '".$countercat." (".$month." ".$year.")',
+                                    a.catnumber = a.catnumber + 1
+                                WHERE cl.cl_to = '".$thecountercat."'";
+ 
+                        echo $countercat." ".$month." ".$year." -".$wikiproject;
+                        echo "<br>";
+                        flush();
+                    }//month
+                }//year
+            }//countercat
+        }//wikiproject
+ 
+        //close connection
+            mysql_close($con)
+                    or die('Could not close connection to db: ' . mysql_error());
+ 
+ //print out to Wikipedia
+ 
+        ?>
