0c2a0ca0c2e84a225a173b84011d173d481a2e4e
Petr Onderka <gsvick@gmail.com>
2012-03-11 18:39:37 +0000

    updated SGE configuration

--- CreateCleanupListing.sh
+++ CreateCleanupListing.sh
@@ -3,6 +3,8 @@
 #$ -j y
 #$ -N CleanupListing
 #$ -m e
-#$ -l sqlprocs-s1=1
+#$ -l sql-s1-user=1
+#$ -l h_rt=3:00:00
+#$ -l virtual_free=100M
 
 php /home/svick/CleanupListing/CreateCleanupListing.php
